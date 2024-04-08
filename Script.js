60a78244a319befb5f2235b9f960b55790e6b046
Andengy <127649084+Andengy@users.noreply.github.com>
2024-04-08 09:27:07 +0800

    Link java script

--- Script.js
+++ Script.js
@@ -1,3 +1,16 @@
+<script>
+function makeGreen(){
+    document.getElementById('Square').Style.background = 'green';
+}
+function makeRed(){
+    document.getElementById('Square').Style.background = 'red';
+}
+function makeGrey(){
+    document.getElementbyID('Square').Style.background = 'grey';
+}
+</script>
+
+
 function createBuzzwordPhrase() {
     /* See https://en.wikipedia.org/wiki/List_of_buzzwords */
     let buzz = ["Paradigm-changing", "Multi-tier", "10,000-foot", "Agile", "Customer", "Win-win"];
