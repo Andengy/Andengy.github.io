c14abff45c5969ae3a835a699dca3c6736ee9a6b
Andengy <127649084+Andengy@users.noreply.github.com>
2024-04-19 16:33:36 +0800

    using chatgpt to fix html and javascript

--- Script.js
+++ Script.js
@@ -1,27 +1,22 @@
 // 1. Locate the element(s)
 let square = document.getElementById('square');
+let words = document.getElementById('words');
 
 // 2. Write the functionality needed when event occurs
-function changeColour(color){
+function changeColour(color) {
     square.style.background = color;
 }
 
-
-let words = document.getElementById('word')
-function greeting(){
-    console.log("Welcome to Flatland")
+function greeting() {
+    console.log("Welcome to Flatland");
     words.innerHTML = "Welcome to Flatland.<br> I am Square ";
 }
-words.addEventListener('load', (event) => greeting())
-//words.addEventListner('load, (event) => greeting())
-document.onload = greeting()
+document.addEventListener('DOMContentLoaded', greeting);
 
-function showMessage(){
-    let message = "Build a " + createBuzzwordPhrase
+function showMessage() {
+    let message = "Build a " + createBuzzwordPhrase();
     words.innerHTML = message;
 }
-
-
 
 function createBuzzwordPhrase() {
     /* See https://en.wikipedia.org/wiki/List_of_buzzwords */
@@ -33,12 +28,12 @@ function createBuzzwordPhrase() {
     let idx_act = Math.floor(Math.random() * action.length);
     let idx_out = Math.floor(Math.random() * outcome.length);
 
-    return  buzz[idx_buz] + " " + action[idx_act] + " " + outcome[idx_out];
+    return buzz[idx_buz] + " " + action[idx_act] + " " + outcome[idx_out];
 }
 
-//3. link element, event to the functionality
+// 3. link element, event to the functionality
 // ie add events
 
-square.addEventListener('click', () => ShowMessage());
+square.addEventListener('click', showMessage);
 square.addEventListener('mouseover', () => changeColour('grey'));
 square.addEventListener('mouseout', () => changeColour('red'));
